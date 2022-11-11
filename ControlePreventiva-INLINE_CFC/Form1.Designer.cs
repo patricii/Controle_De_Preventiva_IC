a9aaf023cb4d0d0511e9134fea43e49375260d99
jagapatr <jagapatr@motorola.com>
2022-11-11 09:35:18 -0300

    Fisrt commit

---
+++ ControlePreventiva-INLINE_CFC/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ControlePreventiva_INLINE_CFC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.user = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Usuário:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Senha:";
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(112, 151);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(148, 20);
+            this.user.TabIndex = 3;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(112, 184);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(148, 20);
+            this.password.TabIndex = 4;
+            this.password.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 39);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Logar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ControlePreventiva_INLINE_CFC.Properties.Resources.FLEX_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(34, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(293, 133);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 232);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Controle Preventiva INLINE / CFC v1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ControlePreventiva-INLINE_CFC/Form2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ControlePreventiva_INLINE_CFC
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'controlePreventiva_ICDataSet.ControlePreventivasIC' table. You can move, or remove it, as needed.
+            this.controlePreventivasICTableAdapter.Fill(this.controlePreventiva_ICDataSet.ControlePreventivasIC);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                controlePreventivasICBindingSource.MovePrevious();
+            }
+            catch {
+                MessageBox.Show("Não foi possivel conectar com o Banco de Dados!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                controlePreventivasICBindingSource.MoveNext();
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possivel conectar com o Banco de Dados!");
+            }
+        }
+
+        private void deletar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                controlePreventivasICBindingSource.RemoveCurrent();
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possivel conectar com o Banco de Dados!");
+            }
+        }
+
+        private void incluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                controlePreventivasICBindingSource.AddNew();
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possivel conectar com o Banco de Dados!");
+            }
+        }
+
+        private void salvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                controlePreventivasICBindingSource.EndEdit();
+                controlePreventivasICTableAdapter.Update(controlePreventiva_ICDataSet);
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possivel conectar com o Banco de Dados!");
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
